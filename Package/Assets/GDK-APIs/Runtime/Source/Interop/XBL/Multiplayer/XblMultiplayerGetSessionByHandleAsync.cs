54895be88a76327a9bc86a82e0555c49f7305002
Michael Copley <micopley@microsoft.com>
2021-07-06 15:26:02 -0700

    added native API commenting for the Interop functions.

--- Package/Assets/GDK-APIs/Runtime/Source/Interop/XBL/Multiplayer/XblMultiplayerGetSessionByHandleAsync.cs
+++ Package/Assets/GDK-APIs/Runtime/Source/Interop/XBL/Multiplayer/XblMultiplayerGetSessionByHandleAsync.cs
@@ -5,13 +5,22 @@ namespace XGamingRuntime.Interop
 {
     internal partial class XblInterop
     {
+        //STDAPI XblMultiplayerGetSessionByHandleAsync(
+        //    _In_ XblContextHandle xblContext,
+        //    _In_ const char* handleId,
+        //    _Inout_ XAsyncBlock* async  
+        //) XBL_NOEXCEPT;
         [DllImport(ThunkDllName, CallingConvention = CallingConvention.StdCall)]
         public static extern Int32 XblMultiplayerGetSessionByHandleAsync(
             XblContextHandle xblContext,  
             byte[] handleId,  
             XAsyncBlockPtr async  
         );
-        
+
+        //STDAPI XblMultiplayerGetSessionByHandleResult(
+        //    _Inout_ XAsyncBlock* async,
+        //    _Out_ XblMultiplayerSessionHandle* handle
+        //) XBL_NOEXCEPT;
         [DllImport(ThunkDllName, CallingConvention = CallingConvention.StdCall)]
         public static extern Int32 XblMultiplayerGetSessionByHandleResult(
             XAsyncBlockPtr async,
