9ed2aee9bab5b1ab1a89fe549d166802edbf2947
tculotta <60450686+tculotta@users.noreply.github.com>
2022-04-15 12:39:24 -0700

    transitioning to pulling dlls from GDK install instead of providing local copies (#83)

--- Package/Assets/GDK-APIs/Runtime/Source/Interop/__GENERATED/XBL/Social/Social.cs
+++ Package/Assets/GDK-APIs/Runtime/Source/Interop/__GENERATED/XBL/Social/Social.cs
@@ -5,46 +5,46 @@ namespace XGamingRuntime.Interop
 {
     public static unsafe partial class Social
     {
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         [return: NativeTypeName("HRESULT")]
         public static extern int XblSocialGetSocialRelationshipsAsync([NativeTypeName("XblContextHandle")] IntPtr xboxLiveContext, [NativeTypeName("uint64_t")] ulong xboxUserId, XblSocialRelationshipFilter socialRelationshipFilter, [NativeTypeName("size_t")] SizeT startIndex, [NativeTypeName("size_t")] SizeT maxItems, XAsyncBlockPtr async);
 
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         [return: NativeTypeName("HRESULT")]
         public static extern int XblSocialGetSocialRelationshipsResult(XAsyncBlockPtr async, [NativeTypeName("XblSocialRelationshipResultHandle *")] IntPtr* handle);
 
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         [return: NativeTypeName("HRESULT")]
         public static extern int XblSocialRelationshipResultGetRelationships([NativeTypeName("XblSocialRelationshipResultHandle")] IntPtr resultHandle, [NativeTypeName("const XblSocialRelationship **")] XblSocialRelationship** relationships, [NativeTypeName("size_t *")] SizeT* relationshipsCount);
 
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         [return: NativeTypeName("HRESULT")]
         public static extern int XblSocialRelationshipResultHasNext([NativeTypeName("XblSocialRelationshipResultHandle")] IntPtr resultHandle, bool* hasNext);
 
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         [return: NativeTypeName("HRESULT")]
         public static extern int XblSocialRelationshipResultGetTotalCount([NativeTypeName("XblSocialRelationshipResultHandle")] IntPtr resultHandle, [NativeTypeName("size_t *")] SizeT* totalCount);
 
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         [return: NativeTypeName("HRESULT")]
         public static extern int XblSocialRelationshipResultGetNextAsync([NativeTypeName("XblContextHandle")] IntPtr xboxLiveContext, [NativeTypeName("XblSocialRelationshipResultHandle")] IntPtr resultHandle, [NativeTypeName("size_t")] SizeT maxItems, XAsyncBlockPtr async);
 
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         [return: NativeTypeName("HRESULT")]
         public static extern int XblSocialRelationshipResultGetNextResult(XAsyncBlockPtr async, [NativeTypeName("XblSocialRelationshipResultHandle *")] IntPtr* handle);
 
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         [return: NativeTypeName("HRESULT")]
         public static extern int XblSocialRelationshipResultDuplicateHandle([NativeTypeName("XblSocialRelationshipResultHandle")] IntPtr handle, [NativeTypeName("XblSocialRelationshipResultHandle *")] IntPtr* duplicatedHandle);
 
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         public static extern void XblSocialRelationshipResultCloseHandle([NativeTypeName("XblSocialRelationshipResultHandle")] IntPtr handle);
 
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         [return: NativeTypeName("XblFunctionContext")]
         public static extern int XblSocialAddSocialRelationshipChangedHandler([NativeTypeName("XblContextHandle")] IntPtr xboxLiveContext, [NativeTypeName("XblSocialRelationshipChangedHandler")] XblSocialRelationshipChangedHandler handler, IntPtr handlerContext);
 
-        [DllImport("Microsoft_Xbox_Services_141_GDK_C_Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        [DllImport("Microsoft.Xbox.Services.141.GDK.C.Thunks", CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
         [return: NativeTypeName("HRESULT")]
         public static extern int XblSocialRemoveSocialRelationshipChangedHandler([NativeTypeName("XblContextHandle")] IntPtr xboxLiveContext, [NativeTypeName("XblFunctionContext")] int handlerFunctionContext);
     }
